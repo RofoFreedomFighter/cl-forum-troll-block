ed25e2ad498c76a677c019cbff54b87c941f3be0
RofoFreedomFighter <greedware@gmail.com>
2015-02-27 19:04:22 -0800

    Update block.js

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-trolls = [
+trollDefaults = [
     "WorldwideStatement",
     "5pcChikenDnnr",
     "Lions_in_Snowjob",
@@ -8,22 +8,57 @@ trolls = [
     "MorningWood",
     "close-call",
     "GamerGirly",
+    "gov-r-n8t-r",
     "TheNaughtyList",
-
+    "RFF-Is-A-Douchebag",
+    "Julie-in-HR",
+    "prettylilthing",
+    "Urinal-_-Cake"
 ];
 
 function block() {
+    chrome.storage.sync.get({
+        trolls: trollDefaults,
+        blockerEnabled: true
+    }, function (items) {
+        block2(items.trolls, items.blockerEnabled);
+    });
+}
+
+function block2(trolls, enabled) {
+    console.log("foo" + trolls);
+    if (!enabled) {
+        return;
+    }
     nodes = document.getElementsByClassName("hnd");
 
     for (var i = 0; i < nodes.length; i++) {
-        isTroll = trolls.filter(function (item) {  return item == nodes[i].innerText}).length > 0;
+        isTroll = trolls.filter(function (item) {
+            return item == nodes[i].innerText
+        }).length > 0;
         if (isTroll && btoa(nodes[i].innerText) != "Ul9GX0Y=") {
             who = nodes[i].innerText;
-            nodes[i].parentElement.parentElement.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : . . : . . : <i> troll (" + who + ") blocked</i>";
+            indent = nodes[i].parentElement.parentElement.innerText.match(/(: \. \. )+/);
+            if (indent == null) {
+                indent = "&nbsp;";
+                padding = "<a class=\"areaselector\"><span title=\"A troll is pwned.\">PWN</span></a>"
+            } else {
+                indent = indent[0];
+                padding = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ";
+            }
+            nodes[i].parentElement.parentElement.innerHTML = padding + indent + "<i> troll (" + who + ") blocked</i>";
 
         }
 
     }
+}
+
+function setTrolls(trollArray) {
+    trolls = trollArray;
+}
+
+function load_settings() {
+
 }
 
 block();
